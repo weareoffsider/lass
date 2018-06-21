86b20d31dcbdaa06068541d1972f79fcf7dd8838
Dominic Whittle <dom@weekends.ws>
2018-06-21 18:54:20 +1000

    Nested at rules test

---
+++ tests/nested-at-rules.test.js
@@ -0,0 +1,30 @@
+const Lass = require('../index')
+
+test('Multi line properties and variables', () => {
+expect(Lass(
+`
+.component
+  width 300px
+
+  @media (min-width: 768px)
+    width 600px
+    @media (min-resolution: 192dpi)
+      background-image url(/img/retina2x.png)
+
+  @media (min-width: 1280px)
+    width 800px
+`
+)).toBe(
+`
+.component {
+  width: 300px;
+
+  @media (min-width: 768px) {
+    width: 600px;
+    @media (min-resolution: 192dpi) {
+      background-image: url(/img/retina2x.png); }}
+
+  @media (min-width: 1280px) {
+    width: 800px; }}
+`
+)})
