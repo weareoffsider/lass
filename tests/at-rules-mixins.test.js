ac632f86e192da4583527f9327e8cc5adc202eca
Dominic Whittle <dom@weekends.ws>
2018-06-21 19:04:03 +1000

    @rules mixin test created

---
+++ tests/at-rules-mixins.test.js
@@ -0,0 +1,44 @@
+const Lass = require('../index')
+
+test('Multi line properties and variables', () => {
+expect(Lass(
+`
+// Inclusive of the value and above.
+.from(@unitlessPx, @rules)
+  @ems unit( (@unitlessPx / 16), em)
+  @media screen and (min-width: @ems)
+    @rules()
+
+.el
+  +from(768)
+    color red
+
++from(768)
+  h1,
+  h2
+    font-size 120%
+
+  body
+    font-size 18px
+`
+)).toBe(
+`
+// Inclusive of the value and above.
+.from(@unitlessPx, @rules) {
+  @ems: unit( (@unitlessPx / 16), em);
+  @media screen and (min-width: @ems) {
+    @rules(); }}
+
+.el {
+  .from(768, {
+    color: red; });}
+
+.from(768, {
+  h1,
+  h2 {
+    font-size: 120%; }
+
+  body {
+    font-size: 18px; }});
+`
+)})
