cec458900399a5700e982571d1af5366c903aed2
Dominic Whittle <dom@weekends.ws>
2018-07-11 17:27:35 +1000

    Split out stringToLineObjects

--- index.js
+++ index.js
@@ -1,3 +1,4 @@
+const stringToLineObjects = require('./src/stringToLineObjects')
 const ASTCreator = require('./src/ASTCreator')
 const util = require('util')
 
@@ -7,39 +8,11 @@ function Lass (input = '') {
   const COMMA_CHAR = ','
   const INDENT_SYMBOL = SPACE_CHAR.repeat(NUM_INDENT_SPACES)
 
-  const lines = input.split('\n')
-  const linesLength = lines.length
-  const lineObjs = []
-  const lineComments = []
-
-  // Do a first pass of all lines, splitting up indentation, content, and comments
-  for (let i = 0; i < linesLength; i++) {
-    const line = lines[i]
-    const lineNum = i + 1
-
-    const _leadingSpaces = leadingSpaces(line)
-    const indent = _leadingSpaces / NUM_INDENT_SPACES
-    if (_leadingSpaces % NUM_INDENT_SPACES) {
-      console.error(`Line ${lineNum} has invalid indentiation. Should be a multiple of ${NUM_INDENT_SPACES}.`)
-    }
-
-    const [content, comment] = splitOnce(line.trim(), '//')
-    const [prop, val] = splitOnce(content || '', ' ')
-
-    // console.log(lineNum, indent, content, comment)
-
-    lineObjs.push({
-      meaningful: content ? true : false, // isMeaningful @TODO
-      lineNum,
-      indent,
-      content: content.trim(),
-      prop: prop.trim(),
-      val: val.trim(),
-      comment: comment.trim(),
-    })
-
-    lineComments.push(comment.trim() || '')
+  const config = {
+    NUM_INDENT_SPACES
   }
+
+  const lineObjs = stringToLineObjects(input, config)
 
   // Do a second pass to determine the relationship between lines
   // Processing states
@@ -402,13 +375,6 @@ function Lass (input = '') {
     return content + ';'
   }
 
-  function leadingSpaces (str) {
-    const match = str.match(/^\s+/)
-    return ! match
-      ? 0
-      : match[0].length
-  }
-
   function getNextMeaningful (arr, i) {
     let nextObj = arr[i + 1]
     if (nextObj === undefined)
@@ -424,11 +390,6 @@ function Lass (input = '') {
 
 
 // Util
-
-function splitOnce (str, sep = '') {
-  const [first, ...rest] = str.split(sep)
-  return [first, rest.join(sep)]
-}
 
 function padStart (str, padString, length) {
   while (str.length < length)
