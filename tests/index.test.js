0927ade07e0fa740b7b3f6a4a5fba346698cfdb4
Dominic Whittle <dom@weekends.ws>
2018-07-14 23:32:42 +1000

    Refactor tests

---
+++ tests/index.test.js
@@ -0,0 +1,24 @@
+const Lass = require('../index')
+const fs = require('fs')
+const path = require('path')
+
+
+runtest('at-rules')
+runtest('many')
+runtest('many-more')
+runtest('mixins-with-rules')
+runtest('multi-line-properties')
+runtest('plugin-lists-objects')
+runtest('properties')
+
+
+function runtest (name) {
+  const less = fs.readFileSync(path.resolve(__dirname, `./less/${name}.less`), 'utf8')
+  const lass = fs.readFileSync(path.resolve(__dirname, `./lass/${name}.lass`), 'utf8')
+  // Jest
+  test(name, () => {
+    expect(Lass(lass)).toBe(less);
+  })
+}
+
+
