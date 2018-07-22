42d5a6605e0455f4ca88c59060e9a9966b6975f3
Dominic Whittle <dom@weekends.ws>
2018-07-22 17:45:27 +1000

    lass plugin for less. Full css compilation, follows imports

---
+++ working/cli.js
@@ -0,0 +1,44 @@
+#! /usr/bin/env node
+
+const lass = require('../index')
+const less = require('less')
+const lessPluginLists = require('less-plugin-lists')
+
+const argv = require('yargs').argv
+const fs = require('fs')
+const path = require('path')
+
+
+const lassPlugin = {
+  priority: 1, // 1 = before import, 1000 = import, 2000 = after import
+  install: function(less, pluginManager) {
+    pluginManager.addPreProcessor({
+      process: lass.render
+    }, this.priority);
+  }
+}
+
+const listsPlugin = new lessPluginLists()
+
+
+if (argv._ && argv._.length > 1) {
+  const filein = argv._[0];
+  const fileout = argv._[1];
+  const str = fs.readFileSync(path.resolve(__dirname, `${filein}`), 'utf8')
+
+  less
+    .render(str, {
+      plugins: [ lassPlugin, listsPlugin ],
+      strictMath: true,
+    })
+    .then((out) => {
+      fs.writeFileSync(
+        path.resolve(__dirname, `${fileout}`),
+        out.css,
+        'utf8'
+      )
+    })
+
+} else {
+  console.error('{in} {out}')
+}
